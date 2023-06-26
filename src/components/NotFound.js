dae3501ef484b59585166fb13417fab3a7240676
Suzan Dewitz <suzanyaa@yahoo.com>
2023-06-26 08:08:06 +0000

    Remove the unused import of 'NoResultsImage

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Asset from './Asset'; // Import the 'Asset' component from the correct file path
 import NoResultsImage from '../assets/no-results.png';
 import styles from '../styles/NotFound.module.css';
 
+// 404 page
 const NotFound = () => {
   return (
     <div className={styles.Margin}>
-      <img src={NoResultsImage} alt="No Results" className={styles.Image} />
-      <p>Sorry, the page you're looking for doesn't exist</p>
+      <Asset
+        src={NoResultsImage}
+        message={`Sorry, the page you're looking for doesn't exist`}
+      />
     </div>
   );
 };
